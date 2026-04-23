6b4aa6849b1d69d18fbe8f577c54e0a892dfd35a
Somya Saxena <somya040706@gmail.com>
2026-04-23 21:34:12 +0530

    usermanagement2.java

--- src/main/java/in/scalive/usermanagement2.java
+++ src/main/java/in/scalive/usermanagement2.java
@@ -3,14 +3,7 @@ package in.scalive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Entry point of the User Management Spring Boot application.
- *
- * @SpringBootApplication enables:
- *   - @Configuration       (marks this as a config class)
- *   - @EnableAutoConfiguration (auto-configures Spring beans)
- *   - @ComponentScan       (scans all sub-packages automatically)
- */
+
 @SpringBootApplication
 public class usermanagement2{
 
